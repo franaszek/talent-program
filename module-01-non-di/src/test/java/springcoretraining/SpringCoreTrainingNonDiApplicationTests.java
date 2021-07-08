9df92da11752aa63e2f4e43759a833872167e064
Y0LX <y0lx@nykredit.dk>
2021-07-08 14:01:36 +0200

    names refactor

--- module-01-non-di/src/test/java/springcoretraining/SpringCoreTrainingNonDiApplicationTests.java
+++ module-01-non-di/src/test/java/springcoretraining/SpringCoreTrainingNonDiApplicationTests.java
@@ -12,7 +12,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class SpringCoreTrainingApplicationTests {
+public class SpringCoreTrainingNonDiApplicationTests {
 
 	@LocalServerPort
 	private int port;
